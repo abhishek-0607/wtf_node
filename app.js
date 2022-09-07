70eebf7feeae8fdf1a46fd590f190a3e8e9a9ffb
Abhishek Silawat <absilawat@gmail.com>
2022-09-07 13:34:00 +0530

    final commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const connect = require("./src/configs/db");
 
 const { register, login } = require("./src/controllers/authController");
 
@@ -9,4 +10,11 @@ app.use(express.json());
 app.post("/register", register);
 app.post("/login", login);
 
-module.exports = app;
+require("dotenv").config();
+
+const port = process.env.PORT || 8888;
+
+app.listen(port, async function () {
+  await connect();
+  console.log(`Listening to port ${port}`);
+});
